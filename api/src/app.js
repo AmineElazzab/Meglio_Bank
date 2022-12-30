6a74f9d52ef2583e828ebfdadbe6e1d5c4c62f57
mohammed amine elazzab <ma.elazzab@gmail.com>
2022-12-30 11:34:30 +0100

    global middlewares & routes & 404 & Error handler & init db with server & initialize server

---
+++ api/src/app.js
@@ -0,0 +1,40 @@
+import 'dotenv/config';
+import { json, urlencoded } from 'express';
+import cors from 'cors';
+import { app, startServer } from './config';
+import { errorHandler, notFound } from './middleware';
+import {
+  AdminRoutes,
+  UsersRoutes,
+} from './routes';
+
+export const init = () => {
+  //global middlewares
+
+  app.use(cors({ origin: '*' }));
+  app.use(json());
+  app.use(urlencoded({ extended: true }));
+
+  //routes
+
+  app.use('/api/auth', UsersRoutes);
+  app.use('/api/admin', AdminRoutes);
+  app.get('/', (req, res) => {
+    console.log('health check');
+    res.json({
+      healthCheck: 'system is ready',
+    });
+  });
+
+  // 404
+  app.use(notFound);
+
+  // Error handler
+  app.use(errorHandler);
+
+  //init db with server
+  startServer();
+};
+
+//initialize server
+init();
