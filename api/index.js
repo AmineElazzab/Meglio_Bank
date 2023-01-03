3ddcd84bc0e2e1b9a02bab0d4944f7b9bbdc7242
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-01-03 11:29:04 +0100

    add routes

--- api/index.js
+++ api/index.js
@@ -1,20 +1,26 @@
 const express = require('express');
-const colors = require('colors')
-const cors = require('cors')
-require('dotenv').config();
+const cors = require('cors');
+const colors = require('colors');
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const conectDB = require("./src/config/db");
+const {errorHandler} = require("./src/middleware/errorHandler");
 
-const connectDB = require('./src/config/db');
-const port = process.env.PORT || 5000;
 const app = express();
-
-// Connect Database
-connectDB();
-
-// Init Middleware
 app.use(cors());
 app.use(express.json());
 
-// Define Routes
-app.listen(port, () =>
-    console.log(`☕️ Server is running on port ➔  ${port} 💣`.green.underline.bold))
+// conect to DB
+conectDB();
 
+// routes
+
+app.use("/api/users", require("./src/routes/UsersRoutes"));
+
+// error handler
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`.yellow.bold);
+    }
+);
