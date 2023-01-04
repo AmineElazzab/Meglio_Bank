896a23be25af2a876ba3914e8f9b65a492fdf7d1
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-01-04 13:55:22 +0100

    User Routes

--- api/src/routes/UsersRoutes.js
+++ api/src/routes/UsersRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express();
-const { Register, Login} = require("../controllers/authController");
+const { Register, Login, GetAllUsers, GetUserById, UpdateUser, DeleteUser } = require("../controllers/authController");
 
 router.post("/register", Register);
 router.post("/login", Login);
+router.get("/users", GetAllUsers);
+router.get("/user/:id", GetUserById);
+router.put("/updateuser/:id", UpdateUser);
+router.delete("/deleteuser/:id", DeleteUser);
+
 
 module.exports = router;
