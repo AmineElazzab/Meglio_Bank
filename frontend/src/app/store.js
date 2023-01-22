1478a30da9dcdbb1d323e81816020cb4a81cd1e2
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-01-22 15:35:51 +0100

    Store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import accountReducer from "../features/accounts/accountSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    auth: authReducer,
+    accounts: accountReducer,
   },
 });
