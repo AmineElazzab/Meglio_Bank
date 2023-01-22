816347d57b1a3f89e6ca44549c8c56139c2ecebe
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-01-22 14:17:48 +0100

    Account Service

---
+++ frontend/src/features/accounts/accountService.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/accounts/";
+
+// Create account
+const createAccount = async (accountData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.post(
+    API_URL + "create-account",
+    accountData,
+    config
+  );
+  return response.data;
+};
+
+// Get account
+const getAccount = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(API_URL + "get-accounts", config);
+  return response.data;
+};
+
+// Delete user account
+const deleteAccount = async (accountId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.delete(
+    API_URL + "/delete-account/" + accountId,
+    config
+  );
+
+  return response.data;
+};
+
+const accountService = {
+  createAccount,
+  getAccount,
+  deleteAccount,
+};
+
+export default accountService;
