8e2a7c37db32bb3b0183a1411d198fd72824e227
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-01-22 14:18:46 +0100

    Auth Service

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/auth/";
+
+// Register user
+const register = async (userData) => {
+  const date = new Date();
+  const response = await axios.post(
+    "http://localhost:8080/api/auth/create-user",
+    userData
+  );
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+    localStorage.setItem("token", response.data.token);
+    localStorage.setItem("token_date", date.getTime());
+  }
+
+  return response.data;
+};
+
+// Login user
+const login = async (userData) => {
+  const date = new Date();
+  const response = await axios.post(API_URL + "login", userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+    localStorage.setItem("token", response.data.token);
+    localStorage.setItem("token_date", date.getTime());
+  }
+
+  return response.data;
+};
+
+// Logout user
+const logout = () => {
+  localStorage.clear();
+};
+
+const authService = {
+  register,
+  logout,
+  login,
+};
+
+export default authService;
