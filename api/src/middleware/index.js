1f9500bdea35edae1a3a8bfcb242053026bf3e93
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-01-22 15:29:39 +0100

    delete

--- api/src/middleware/index.js
+++
@@ -1,7 +0,0 @@
-export { notFound } from './notFound';
-export { errorHandler } from './errorHandler';
-export { checkToken } from './adminMiddleware';
-export { checkUserToken } from './authMiddleware';
-export { nodemailer } from './nodemailer';
-export { transporter } from './nodemailer';
- 
