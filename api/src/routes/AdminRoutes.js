fb2f0a26427b7e8d1a0dcf391ade52390dd2e8cf
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-01-22 15:30:52 +0100

    delete

--- api/src/routes/AdminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express();
-const {RegisterAdmin, LoginAdmin, GetAllUsers} = require("../controllers/adminController");
-
-router.post("/register", RegisterAdmin);
-router.post("/login", LoginAdmin);
-router.get("/users", GetAllUsers);
-
-module.exports = router;
